Amin-Musharab/discourse

commit 999e2ff514b15c850c027cbf8f4d955d6ad01733

UX: Show vertical topic timeline while composing (#8813)
Keeps topic timeline visible if space permits when composing, and adjusts its height dynamically.

app/assets/javascripts/discourse/components/topic-timeline.js.es6
@@ -103,5 +103,18 @@ export default MountWidget.extend(Docking, {
 
     this.dispatch("topic:current-post-scrolled", "timeline-scrollarea");
     this.dispatch("topic:toggle-actions", "topic-admin-menu-button");
+    if (!this.site.mobileView) {
+      this.appEvents.on("composer:opened", this, this.queueRerender);
+      this.appEvents.on("composer:resized", this, this.queueRerender);
+      this.appEvents.on("composer:closed", this, this.queueRerender);
+    }
+  },
+
+  willDestroyElement() {
+    if (!this.site.mobileView) {
+      this.appEvents.off("composer:opened", this, this.queueRerender);
+      this.appEvents.off("composer:resized", this, this.queueRerender);
+      this.appEvents.off("composer:closed", this, this.queueRerender);
+    }
   }
 });

app/assets/javascripts/discourse/widgets/topic-timeline.js.es6
@@ -10,9 +10,22 @@ import renderTopicFeaturedLink from "discourse/lib/render-topic-featured-link";
 
 const SCROLLER_HEIGHT = 50;
 const LAST_READ_HEIGHT = 20;
+const MIN_SCROLLAREA_HEIGHT = 170;
+const MAX_SCROLLAREA_HEIGHT = 300;
 
 function scrollareaHeight() {
-  return $(window).height() < 425 ? 150 : 300;
+  const composerHeight =
+      document.getElementById("reply-control").offsetHeight || 0,
+    headerHeight = document.querySelectorAll(".d-header")[0].offsetHeight || 0;
+
+  // scrollarea takes up about half of the timeline's height
+  const availableHeight =
+    (window.innerHeight - composerHeight - headerHeight) / 2;
+
+  return Math.max(
+    MIN_SCROLLAREA_HEIGHT,
+    Math.min(availableHeight, MAX_SCROLLAREA_HEIGHT)
+  );
 }
 
 function scrollareaRemaining() {

app/assets/javascripts/discourse/components/topic-navigation.js.es6
@@ -1,6 +1,5 @@
 import EmberObject from "@ember/object";
-import { scheduleOnce } from "@ember/runloop";
-import { later } from "@ember/runloop";
+import { debounce, later } from "@ember/runloop";
 import Component from "@ember/component";
 import { observes } from "discourse-common/utils/decorators";
 import showModal from "discourse/lib/show-modal";
@@ -9,6 +8,8 @@ import PanEvents, {
   SWIPE_DISTANCE_THRESHOLD,
   SWIPE_VELOCITY_THRESHOLD
 } from "discourse/mixins/pan-events";
+
+const MIN_WIDTH_TIMELINE = 924;
 
 export default Component.extend(PanEvents, {
   composerOpen: null,
@@ -36,14 +37,20 @@ export default Component.extend(PanEvents, {
       let renderTimeline = !this.site.mobileView;
 
       if (renderTimeline) {
-        const width = $(window).width();
-        let height = $(window).height();
+        const width = window.innerWidth,
+          composer = document.getElementById("reply-control"),
+          timelineContainer = document.querySelectorAll(
+            ".timeline-container"
+          )[0],
+          headerHeight =
+            document.querySelectorAll(".d-header")[0].offsetHeight || 0;
 
-        if (this.composerOpen) {
-          height -= $("#reply-control").height();
+        if (timelineContainer && composer) {
+          renderTimeline =
+            width > MIN_WIDTH_TIMELINE &&
+            window.innerHeight - composer.offsetHeight - headerHeight >
+              timelineContainer.offsetHeight;
         }
-
-        renderTimeline = width > 924 && height > 520;
       }
 
       info.setProperties({
@@ -54,7 +61,7 @@ export default Component.extend(PanEvents, {
   },
 
   _checkSize() {
-    scheduleOnce("afterRender", this, this._performCheckSize);
+    debounce(this, this._performCheckSize, 300);
   },
 
   // we need to store this so topic progress has something to init with
@@ -89,8 +96,7 @@ export default Component.extend(PanEvents, {
 
   composerOpened() {
     this.set("composerOpen", true);
-    // we need to do the check after animation is done
-    later(() => this._checkSize(), 500);
+    this._checkSize();
   },
 
   composerClosed() {
@@ -191,8 +197,9 @@ export default Component.extend(PanEvents, {
       $(window).on("resize.discourse-topic-navigation", () =>
         this._checkSize()
       );
-      this.appEvents.on("composer:will-open", this, this.composerOpened);
-      this.appEvents.on("composer:will-close", this, this.composerClosed);
+      this.appEvents.on("composer:opened", this, this.composerOpened);
+      this.appEvents.on("composer:resized", this, this.composerOpened);
+      this.appEvents.on("composer:closed", this, this.composerClosed);
       $("#reply-control").on("div-resized.discourse-topic-navigation", () =>
         this._checkSize()
       );
@@ -213,8 +220,9 @@ export default Component.extend(PanEvents, {
 
     if (!this.site.mobileView) {
       $(window).off("resize.discourse-topic-navigation");
-      this.appEvents.off("composer:will-open", this, this.composerOpened);
-      this.appEvents.off("composer:will-close", this, this.composerClosed);
+      this.appEvents.off("composer:opened", this, this.composerOpened);
+      this.appEvents.off("composer:resized", this, this.composerOpened);
+      this.appEvents.off("composer:closed", this, this.composerClosed);
       $("#reply-control").off("div-resized.discourse-topic-navigation");
     }
   }

commit 1aa56a0f6341855132c51c5b6ed9b8801ce07869

DEV: Upgrade Ember to version 3.12.2 (#8753)
* DEV: Use Ember 3.12.2
* Add Ember version to ThemeField's DEPENDENT_CONSTANTS
* DEV: Use `id` instead of `elementId` (See: https://github.com/emberjs/ember.js/issues/18147)
* FIX: Don't leak event listeners (bug introduced in 999e2ff)

Gemfile
@@ -65,7 +65,7 @@ gem 'http_accept_language', require: false
 # Ember related gems need to be pinned cause they control client side
 # behavior, we will push these versions up when upgrading ember
 gem 'ember-rails', '0.18.5'
-gem 'discourse-ember-source', '~> 3.10.0'
+gem 'discourse-ember-source', '~> 3.12.2'
 gem 'ember-handlebars-template', '0.8.0'
 
 gem 'barber'

app/assets/javascripts/discourse/components/topic-timeline.js.es6
@@ -111,6 +111,8 @@ export default MountWidget.extend(Docking, {
   },
 
   willDestroyElement() {
+    this._super(...arguments);
+
     if (!this.site.mobileView) {
       this.appEvents.off("composer:opened", this, this.queueRerender);
       this.appEvents.off("composer:resized", this, this.queueRerender);

app/assets/javascripts/pretty-text/emoji.js.es6
@@ -126,8 +126,6 @@ export function performEmojiUnescape(string, opts) {
         } alt='${emojiVal}' class='${classes}'>`
       : m;
   });
-
-  return string;
 }
 
 export function performEmojiEscape(string, opts) {
@@ -145,8 +143,6 @@ export function performEmojiEscape(string, opts) {
 
     return m;
   });
-
-  return string;
 }
 
 export function isCustomEmoji(code, opts) {

app/assets/javascripts/wizard/templates/components/wizard-field-dropdown.hbs
@@ -1,5 +1,5 @@
 {{combo-box
-  elementId=field.id
+  id=field.id
   class=fieldClass
   value=field.value
   content=field.choices

app/assets/javascripts/wizard/templates/components/wizard-field-text.hbs
@@ -1,1 +1,1 @@
-{{input elementId=field.id value=field.value class=fieldClass placeholder=field.placeholder tabindex="9"}}
+{{input id=field.id value=field.value class=fieldClass placeholder=field.placeholder tabindex="9"}}

app/assets/javascripts/wizard/templates/components/wizard-field-textarea.hbs
@@ -1,1 +1,1 @@
-{{textarea elementId=field.id value=field.value class=fieldClass placeholder=field.placeholder tabindex="9"}}
+{{textarea id=field.id value=field.value class=fieldClass placeholder=field.placeholder tabindex="9"}}

app/models/theme_field.rb
@@ -61,8 +61,11 @@ class ThemeField < ActiveRecord::Base
                    if: Proc.new { |field| ThemeField.theme_var_type_ids.include?(field.type_id) }
 
   BASE_COMPILER_VERSION = 14
-  DEPENDENT_CONSTANTS = [BASE_COMPILER_VERSION,
-                        GlobalSetting.cdn_url]
+  DEPENDENT_CONSTANTS = [
+    BASE_COMPILER_VERSION,
+    Ember::VERSION,
+    GlobalSetting.cdn_url
+  ]
   COMPILER_VERSION = Digest::SHA1.hexdigest(DEPENDENT_CONSTANTS.join)
 
   belongs_to :theme

spec/lib/theme_javascript_compiler_spec.rb
@@ -74,6 +74,11 @@ describe ThemeJavascriptCompiler do
     # For the Ember (Glimmer) templates, serverside rendering is not trivial,
     # so check the compiled JSON against known working output
     let(:compiler) { described_class.new(theme_id) }
+    let(:helper_opcode) do
+      append = statement("{{dummy-helper 1}}")[0]
+      helper = append[1]
+      helper[0]
+    end
 
     def statement(template)
       compiled = compiler.compile(template)
@@ -82,28 +87,26 @@ describe ThemeJavascriptCompiler do
       block["statements"]
     end
 
-    # might change/break when updating ember
-    EMBER_INTERNAL_ID = 29
     it 'adds the theme id to the helpers' do
       expect(statement("{{theme-prefix 'translation_key'}}")).
-        to eq([[1, [EMBER_INTERNAL_ID, "theme-prefix", [22, "translation_key"], nil], false]])
+        to eq([[1, [helper_opcode, "theme-prefix", [22, "translation_key"], nil], false]])
       expect(statement("{{theme-i18n 'translation_key'}}")).
-        to eq([[1, [EMBER_INTERNAL_ID, "theme-i18n", [22, "translation_key"], nil], false]])
+        to eq([[1, [helper_opcode, "theme-i18n", [22, "translation_key"], nil], false]])
       expect(statement("{{theme-setting 'setting_key'}}")).
-        to eq([[1, [EMBER_INTERNAL_ID, "theme-setting", [22, "setting_key"], nil], false]])
+        to eq([[1, [helper_opcode, "theme-setting", [22, "setting_key"], nil], false]])
 
       # Works when used inside other statements
       expect(statement("{{dummy-helper (theme-prefix 'translation_key')}}")).
-        to eq([[1, [EMBER_INTERNAL_ID, "dummy-helper", [[EMBER_INTERNAL_ID, "theme-prefix", [22, "translation_key"], nil]], nil], false]])
+        to eq([[1, [helper_opcode, "dummy-helper", [[helper_opcode, "theme-prefix", [22, "translation_key"], nil]], nil], false]])
     end
 
     it 'works with the old settings syntax' do
       expect(statement("{{themeSettings.setting_key}}")).
-        to eq([[1, [EMBER_INTERNAL_ID, "theme-setting", [22, "setting_key"], [["deprecated"], [true]]], false]])
+        to eq([[1, [helper_opcode, "theme-setting", [22, "setting_key"], [["deprecated"], [true]]], false]])
 
       # Works when used inside other statements
       expect(statement("{{dummy-helper themeSettings.setting_key}}")).
-        to eq([[1, [EMBER_INTERNAL_ID, "dummy-helper", [[EMBER_INTERNAL_ID, "theme-setting", [22, "setting_key"], [["deprecated"], [true]]]], nil], false]])
+        to eq([[1, [helper_opcode, "dummy-helper", [[helper_opcode, "theme-setting", [22, "setting_key"], [["deprecated"], [true]]]], nil], false]])
     end
   end
 