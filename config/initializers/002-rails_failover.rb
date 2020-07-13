Amin-Musharab/discourse

commit d5c56a846a358277a232f7389a47808db9fa624d

DEV: Only failover the entire cluster when the default db goes down.

config/initializers/002-rails_failover.rb
@@ -31,9 +31,11 @@ if defined?(RailsFailover::ActiveRecord)
   end
 
   RailsFailover::ActiveRecord.on_failover do
-    RailsMultisite::ConnectionManagement.each_connection do
-      Discourse.enable_readonly_mode(Discourse::PG_READONLY_MODE_KEY)
-      Sidekiq.pause!("pg_failover") if !Sidekiq.paused?
+    if RailsMultisite::ConnectionManagement.current_db = RailsMultisite::ConnectionManagement::DEFAULT
+      RailsMultisite::ConnectionManagement.each_connection do
+        Discourse.enable_readonly_mode(Discourse::PG_READONLY_MODE_KEY)
+        Sidekiq.pause!("pg_failover") if !Sidekiq.paused?
+      end
     end
   end
 

commit 00efeda566e30cce9b7169952eed52b0bf247b91

DEV: Fix error introduced in d5c56a846.

config/initializers/002-rails_failover.rb
@@ -31,7 +31,7 @@ if defined?(RailsFailover::ActiveRecord)
   end
 
   RailsFailover::ActiveRecord.on_failover do
-    if RailsMultisite::ConnectionManagement.current_db = RailsMultisite::ConnectionManagement::DEFAULT
+    if RailsMultisite::ConnectionManagement.current_db == RailsMultisite::ConnectionManagement::DEFAULT
       RailsMultisite::ConnectionManagement.each_connection do
         Discourse.enable_readonly_mode(Discourse::PG_READONLY_MODE_KEY)
         Sidekiq.pause!("pg_failover") if !Sidekiq.paused?